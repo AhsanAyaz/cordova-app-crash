6b84f459d1232f5c59328d1bbb195701aa757498
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2018-10-15 00:20:46 +0500

    Fixed the ios plugin and added Readme

--- src/ios/AppCrash.m
+++ src/ios/AppCrash.m
@@ -11,18 +11,17 @@
 
 @implementation AppCrash
 
-- (void)AppCrash:(CDVInvokedUrlCommand*)command
-{
+- (void)crash:(CDVInvokedUrlCommand *)command{
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-
+    
     if (echo != nil && [echo length] > 0) {
         int *x = NULL; *x = 42;
         exit(0);
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
-
+    
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
